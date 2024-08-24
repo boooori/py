1f6c9e7c376fe89e5e589b1f4c8a41e85d74d0fc
boooori <138274199+boooori@users.noreply.github.com>
2024-08-24 11:30:44 +0900

    Add files via upload

---
+++ C25.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+	/*
+	srand(time(NULL));
+	int random = 0;
+	int num[0];
+	for(int i = 0;i<10;i++)
+		random = rand() % 9;
+		num[i] = {random};
+		printf("%d",num);
+		*/
+	int num1,num2 = 0;
+	scanf("%d,%d",&num1,&num2);
+	return 0;
+	
+}
